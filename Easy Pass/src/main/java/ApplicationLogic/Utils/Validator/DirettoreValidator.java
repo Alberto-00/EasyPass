870c7ceba4374de0b6d891ba66b092b10e770a6a
Alberto-00 <albmont99@gmail.com>
2021-12-31 12:51:26 +0100

    Login - registrazione
    fix bug email/username

--- Easy Pass/src/main/java/ApplicationLogic/Utils/Validator/DirettoreValidator.java
+++ Easy Pass/src/main/java/ApplicationLogic/Utils/Validator/DirettoreValidator.java
@@ -1,7 +1,5 @@
 package ApplicationLogic.Utils.Validator;
 
-import ApplicationLogic.Utils.Validator.Validator;
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.regex.Pattern;
 
@@ -9,7 +7,7 @@ public class DirettoreValidator {
 
     public static Validator validateSigin(HttpServletRequest request){
         Validator validator = new Validator(request);
-        validator.assertMatch("email", Pattern.compile("^[a-zA-Z0-9_.]+@(?:(?:[a-zA-Z0-9-]+\\.)?[a-zA-Z]+\\.)?(unisa)\\.it$"), "L’e-mail inserita non è corretta");
+        validator.assertMatch("email", Pattern.compile("^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){2,18}[a-zA-Z0-9]$|^[a-zA-Z0-9_.+-]{3,}@(?:(?:[a-zA-Z0-9-]+\\.)?[a-zA-Z]+\\.)?(unisa)\\.it$"), "L’e-mail inserita non è corretta");
         validator.assertMatch("password", Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[=^ ì{}+çò°àù§èé#@$!%€*?&:,;'._<>|-])[A-Za-z\\d=^ ì{}+çò°àù§èé#@$!%€*?&:,;'._<>|-]{8,20}$"),"La password inserita non è corretta");
         return validator;
     }
